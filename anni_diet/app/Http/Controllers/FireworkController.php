5a25a3b14ee8dd9b313eaaea4390b22186ef747d
David Alvarez <devdav@pm.me>
2020-07-29 21:41:38 +0200

    Only one token per user is kept

--- anni_diet/app/Http/Controllers/FireworkController.php
+++ anni_diet/app/Http/Controllers/FireworkController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\FireworkEvent;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class FireworkController extends Controller
 {
